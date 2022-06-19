9904e4923e12e6434eb835c00d3496c2b93e74dd
opeyemi <solomonope@users.noreply.github.com>
2022-06-19 08:32:40 +0000

    chore: ran poetry update

--- orca/k8s.py
+++ orca/k8s.py
@@ -1,33 +1,29 @@
 from typing import Dict
 from kubernetes import client
 
-
-
 class K8sService:
-
     def __init__(self):
-        v1 = client.CoreV1Api()
-        appsV1  =  client.AppsV1Api()
-        net  =  client.NetworkingV1Api
+        self.coreV1Api = client.CoreV1Api()
+        self.appsV1  =  client.AppsV1Api()
 
     def create_namespace(self, name: str, annotations: Dict[str, str]):
         metadata = client.V1ObjectMeta(name=name, annotations=annotations)
         namespace = client.V1Namespace(metadata=metadata)
-        return v1.create_namespace(body=namespace)
+        self.coreV1Api.g
+        return self.coreV1Api.create_namespace(body=namespace)
         
 
     def create_ingress(self):
         pass
 
     def create_service(self):
-        v1.create_namespaced_service()
         pass
 
     def create_service_account(self, name: str, namespace: str, annotations: Dict[str, str]):
-        return v1.create_namespaced_service_account()
+        return self.coreV1Ap.create_namespaced_service_account()
 
     def create_stateful_set(self):
         client.V1StatefulSetSpec()
         client.V1StatefulSet()
-        self.appsV1.create_namespaced_stateful_set()
+        self.coreV1Api.create_namespaced_stateful_set()
         
